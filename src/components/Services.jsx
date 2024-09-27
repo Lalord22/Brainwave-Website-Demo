dac5736105e7f8e48e7cb35efe0870e276108db4
Lalord22 <lalo22296@hotmail.com>
2024-09-27 16:47:43 -0600

    Adds cards to services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Section from "./Section";
 import Heading from "./Heading";
-import { check, service1, service2 } from "../assets";
+import { check, service1, service2, service3 } from "../assets";
 import { brainwaveServicesIcons, brainwaveServices } from "../constants";
 import Generating from "./Generating";
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "./design/Services";
 
 function Services() {
   return (
@@ -81,7 +87,57 @@ function Services() {
                 </p>
               </div>
             </div>
+
+            <div
+              className="p-4 bg-n-7 rounded-3xl 
+            overflow-hidden lg:min-h-[46rem]"
+            >
+              <div className="py-12 px-4 xl:px-8">
+                <h4 className="h4 mb-4">Video Generation</h4>
+                <p className="body-2 mb-[2rem] text-n-3">
+                  The world's first AI-powered video generation tool
+                </p>
+                <ul className="flex items-center justify-between">
+                  {brainwaveServicesIcons.map((item, index) => (
+                    <li
+                      key={index}
+                      className={`rounded-2xl flex items-center justify-center
+                    ${
+                      index === 2
+                        ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]"
+                        : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
+                    }`}
+                    >
+                      <div
+                        className={
+                          index === 2
+                            ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
+                            : ""
+                        }
+                      >
+                        <img src={item} width={24} height={24} alt={item} />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div
+                className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden
+              md:h-[25rem]"
+              >
+                <img
+                  src={service3}
+                  alt="robot3"
+                  className="w-full h-full object-cover"
+                  height={400}
+                  width={520}
+                />
+                <VideoChatMessage />
+                <VideoBar />
+              </div>
+            </div>
           </div>
+          <Gradient />
         </div>
       </div>
     </Section>
