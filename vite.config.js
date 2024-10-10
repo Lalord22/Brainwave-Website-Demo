cf24afcdb284efdafa5b0dd232a9b7ade7e2bebb
Lalord22 <lalo22296@hotmail.com>
2024-10-10 13:04:22 -0600

    Redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/Brainwave-Website-Demo/",
+  build: {
+    assetsDir: "./assets", // Fuerza las rutas relativas en los recursos
+  },
   plugins: [react()],
 });
