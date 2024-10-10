4f3219a8e42ba3eb50f1e5e0cca17ed6d6fefbb8
Lalord22 <lalo22296@hotmail.com>
2024-10-10 13:01:33 -0600

    Set up for pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,11 @@ import Services from "./components/Services";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Footer from "./components/Footer";
+import { HashRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
@@ -26,7 +27,7 @@ const App = () => {
         <Footer />
       </div>
       <ButtonGradient />
-    </>
+    </Router>
   );
 };
 
